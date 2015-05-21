c83c772c23aee00f2ec4d64c02ecb73231e5a380
timmtonga <timmtonga@gmail.com>
2015-05-21 14:28:30 +0200

    Added tag version variable and ensured that some files are not tracked byt git

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+APP_VERSION = `git describe`.gsub("\n","")
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
